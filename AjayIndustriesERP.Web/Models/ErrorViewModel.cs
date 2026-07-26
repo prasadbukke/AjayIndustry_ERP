24e36c54c373ae1221b973af15d587402c79accf
Sapana <Sapana@DESKTOP-PSM3ACH>
2026-07-26 21:37:17 +0530

    Initial ERP Commit

---
+++ AjayIndustriesERP.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AjayIndustriesERP.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
